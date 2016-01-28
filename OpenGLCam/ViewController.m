80ddb537e37d5eccf261f3beb0d6602fcbec9bc6
YourtionGuo <yourtion@gmail.com>
2016-01-28 14:40:04 +0800

    添加滤镜

--- OpenGLCam/ViewController.m
+++ OpenGLCam/ViewController.m
@@ -8,38 +8,37 @@
 
 #import "ViewController.h"
 #import <AVFoundation/AVFoundation.h>
+#import <QuartzCore/QuartzCore.h>
+#import <GLKit/GLKit.h>
 
 @interface ViewController ()<AVCaptureVideoDataOutputSampleBufferDelegate>
 @property (nonatomic , strong) AVCaptureVideoPreviewLayer *captureVideoPreviewLayer ;
 @property (nonatomic) int time;
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    GLKView* _glView;
+    EAGLContext* _glContext;
+    CIContext* _ciContext;
+    AVCaptureSession *session ;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-}
+    _glContext = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
+    _ciContext = [CIContext contextWithEAGLContext:_glContext];
+    _glView = [[GLKView alloc] initWithFrame:self.view.frame context:_glContext];
+    _glView.context = _glContext;
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-- (void)viewDidAppear:(BOOL)animated
-{
-    [super viewDidAppear:animated];
-    
-    AVCaptureSession *session = [[AVCaptureSession alloc] init];
+    if (![EAGLContext setCurrentContext:_glContext]) {
+        NSLog(@"Failed to set current OpenGL context");
+        exit(1);
+    }
+    [self.view addSubview:_glView];
+    session = [[AVCaptureSession alloc] init];
     
     session.sessionPreset = AVCaptureSessionPreset640x480;
-    
-    self.captureVideoPreviewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:session];
-    //    self.captureVideoPreviewLayer.frame = self.view.frame ;
-    self.captureVideoPreviewLayer.frame = CGRectMake( 0, 0, 480, 640 );
-    self.captureVideoPreviewLayer.position = self.view.center ;
-    [self.captureVideoPreviewLayer setVideoGravity:AVLayerVideoGravityResizeAspectFill];
-    //    [self.captureVideoPreviewLayer setVideoGravity:AVLayerVideoGravityResize];
-    [self.view.layer addSublayer:self.captureVideoPreviewLayer];
     
     AVCaptureDevice *deviceFront ;
     
@@ -62,28 +61,47 @@
     AVCaptureVideoDataOutput * dataOutput = [[AVCaptureVideoDataOutput alloc] init];
     [dataOutput setAlwaysDiscardsLateVideoFrames:YES];
     [dataOutput setVideoSettings:[NSDictionary dictionaryWithObject:[NSNumber numberWithInt:kCVPixelFormatType_32BGRA] forKey:(id)kCVPixelBufferPixelFormatTypeKey]];
-    [dataOutput setSampleBufferDelegate:self queue:dispatch_get_global_queue(0, 0)];
     
-    [session beginConfiguration];
+    [dataOutput setSampleBufferDelegate:self queue:dispatch_queue_create("Video", DISPATCH_QUEUE_SERIAL)];
+    
+    
     if ([session canAddInput:input]) {
         [session addInput:input];
     }
     if ([session canAddOutput:dataOutput]) {
         [session addOutput:dataOutput];
     }
-    [session commitConfiguration];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    
     
     [session startRunning];
 }
 -(void)captureOutput:(AVCaptureOutput *)captureOutput didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection {
     NSDate *start = [NSDate date];
-    CVPixelBufferRef pixelBuffer = (CVPixelBufferRef)CMSampleBufferGetImageBuffer(sampleBuffer);
-    CVPixelBufferLockBaseAddress(pixelBuffer, 0);
-    uint8_t *baseAddress = CVPixelBufferGetBaseAddress(pixelBuffer);
-    
-    
-    CVPixelBufferUnlockBaseAddress(pixelBuffer, 0);
+    connection.videoOrientation = AVCaptureVideoOrientationPortrait;
+    CVPixelBufferRef pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer);
+    CIImage *image = [CIImage imageWithCVPixelBuffer:pixelBuffer];
+    CIFilter *crystallize = [CIFilter filterWithName:@"CIPhotoEffectChrome" keysAndValues:kCIInputImageKey, image, nil];
+    [crystallize setValue:image forKey:kCIInputImageKey];
+    [_glView bindDrawable];
+    [_ciContext drawImage:crystallize.outputImage inRect:image.extent fromRect:image.extent];
+    [_glView display];
+//    CVPixelBufferLockBaseAddress(pixelBuffer, 0);
+//    uint8_t *baseAddress = CVPixelBufferGetBaseAddress(pixelBuffer);
+//    
+//    
+//    CVPixelBufferUnlockBaseAddress(pixelBuffer, 0);
     self.time = -[start timeIntervalSinceNow]*1000;
+    NSLog(@"time : %d",self.time);
 }
 
 @end
