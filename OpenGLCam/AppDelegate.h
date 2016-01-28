c717a6a050a3e99e6f03ac87b618d6712e091afb
YourtionGuo <yourtion@gmail.com>
2016-01-28 13:15:55 +0800

    Initial Commit

---
+++ OpenGLCam/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenGLCam
+//
+//  Created by YourtionGuo on 1/28/16.
+//  Copyright © 2016 Yourtion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OpenGLCam/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OpenGLCam
+//
+//  Created by YourtionGuo on 1/28/16.
+//  Copyright © 2016 Yourtion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
