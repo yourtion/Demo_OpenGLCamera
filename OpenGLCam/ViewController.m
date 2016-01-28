7cd4b962e482156e02e318c8d1bee8748375040c
YourtionGuo <yourtion@gmail.com>
2016-01-28 13:19:27 +0800

    add AVFoundation

--- OpenGLCam/ViewController.m
+++ OpenGLCam/ViewController.m
@@ -7,9 +7,11 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
-@interface ViewController ()
-
+@interface ViewController ()<AVCaptureVideoDataOutputSampleBufferDelegate>
+@property (nonatomic , strong) AVCaptureVideoPreviewLayer *captureVideoPreviewLayer ;
+@property (nonatomic) int time;
 @end
 
 @implementation ViewController
@@ -22,6 +24,66 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    AVCaptureSession *session = [[AVCaptureSession alloc] init];
+    
+    session.sessionPreset = AVCaptureSessionPreset640x480;
+    
+    self.captureVideoPreviewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:session];
+    //    self.captureVideoPreviewLayer.frame = self.view.frame ;
+    self.captureVideoPreviewLayer.frame = CGRectMake( 0, 0, 480, 640 );
+    self.captureVideoPreviewLayer.position = self.view.center ;
+    [self.captureVideoPreviewLayer setVideoGravity:AVLayerVideoGravityResizeAspectFill];
+    //    [self.captureVideoPreviewLayer setVideoGravity:AVLayerVideoGravityResize];
+    [self.view.layer addSublayer:self.captureVideoPreviewLayer];
+    
+    AVCaptureDevice *deviceFront ;
+    
+    NSArray *devices = [AVCaptureDevice devices];
+    for (AVCaptureDevice *device in devices) {
+        if ([device hasMediaType:AVMediaTypeVideo]) {
+            
+            if ([device position] == AVCaptureDevicePositionFront) {
+                deviceFront = device;
+            }
+        }
+    }
+    
+    NSError *error = nil;
+    AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:deviceFront error:&error];
+    if (!input) {
+        // Handle the error appropriately.
+        NSLog(@"ERROR: trying to open camera: %@", error);
+    }
+    AVCaptureVideoDataOutput * dataOutput = [[AVCaptureVideoDataOutput alloc] init];
+    [dataOutput setAlwaysDiscardsLateVideoFrames:YES];
+    [dataOutput setVideoSettings:[NSDictionary dictionaryWithObject:[NSNumber numberWithInt:kCVPixelFormatType_32BGRA] forKey:(id)kCVPixelBufferPixelFormatTypeKey]];
+    [dataOutput setSampleBufferDelegate:self queue:dispatch_get_global_queue(0, 0)];
+    
+    [session beginConfiguration];
+    if ([session canAddInput:input]) {
+        [session addInput:input];
+    }
+    if ([session canAddOutput:dataOutput]) {
+        [session addOutput:dataOutput];
+    }
+    [session commitConfiguration];
+    
+    [session startRunning];
+}
+-(void)captureOutput:(AVCaptureOutput *)captureOutput didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection {
+    NSDate *start = [NSDate date];
+    CVPixelBufferRef pixelBuffer = (CVPixelBufferRef)CMSampleBufferGetImageBuffer(sampleBuffer);
+    CVPixelBufferLockBaseAddress(pixelBuffer, 0);
+    uint8_t *baseAddress = CVPixelBufferGetBaseAddress(pixelBuffer);
+    
+    
+    CVPixelBufferUnlockBaseAddress(pixelBuffer, 0);
+    self.time = -[start timeIntervalSinceNow]*1000;
 }
 
 @end
